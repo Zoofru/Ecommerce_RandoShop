6ae758e716d1f99a05f165131ebe8ff8e5fe595a
Zoofru <masonbush752@gmail.com>
2021-10-22 18:37:05 -0500

    cart add item

--- backend/controllers/cartcontroller.js
+++ backend/controllers/cartcontroller.js
@@ -1,4 +1,17 @@
+const { modalUnstyledClasses } = require('@mui/core')
 const models = require('../models')
 const cartController = {}
+
+cartController.addItem = async (req, res) => {
+    try {
+        const cart = await models.shoppingcart.create({
+            itemid: req.body.itemId,
+            userid: req.body.userId
+        })
+        res.json({cart})
+    } catch (error) {
+        res.json({error: error.message})
+    }
+}
 
 module.exports = cartController
--- backend/routes/cartroutes.js
+++ backend/routes/cartroutes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const cartController = require('../controllers/cartcontroller')
 const cartRoutes = express.Router()
 
+cartRoutes.post('/add', cartController.addItem)
 
 module.exports = cartRoutes
